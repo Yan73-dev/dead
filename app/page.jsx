8acd8e9317e39e514af75e2b7a7f67b8221bc9ef
yan73-dev <y1763335@gmail.com>
2026-04-12 12:57:52 -0700

    plutonivo

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import supabase from "./supa";
 
 export default function Home() {
 async function handleClick() {
-  await supabase.from('yan').insert({ name: 'John Doe' })}
+  await supabase.insert.into("test").values({ name: "John Doe" }).execute();}
   return <button onClick={handleClick}>Insert</button>
 
 } 
