b9a0503a3e49a7ed12ab02ced527613fed3dd785
yan73-dev <y1763335@gmail.com>
2026-04-12 12:32:04 -0700

    plutonia

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,16 @@
+"use client"
 import Home from "./page";
 
 export default function Layout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-      </head>
-      <body>
-        {children} {/* Cambia <Home /> por {children} */}
-      </body>
-    </html>
-  );
+    return ( <html lang = "en" >
+        <head>
+        <meta charSet = "UTF-8" / >
+        <meta name = "viewport"
+        content = "width=device-width, initial-scale=1.0" / >
+  
+        </head> 
+        <body > { children } { /* Cambia <Home /> por {children} */ } 
+        </body>
+        </html>
+    );
 }
