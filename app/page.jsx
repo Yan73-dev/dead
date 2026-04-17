92c4e4b5f30f2f7653b7a686590cc15b0b5b5dac
yan73-dev <y1763335@gmail.com>
2026-04-16 22:07:49 -0700

    mercurio

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import {supabase} from "./supa";
 export default function Home() {
 async function handleClick() {
   await supabase.from("yan").insert([{name:"name"}])
-  await supa.from("yan").insert([{name:"name"}])
+  //await supa.from("yan").insert([{name:"name"}])
 
 }
   return <> <button onClick={handleClick} style={{color:"red",background:"yellow"}}>Insert</button>
