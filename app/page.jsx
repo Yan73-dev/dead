08fd4e61f753df2abd066f7d21b58e94d10b0ffa
yan73-dev <y1763335@gmail.com>
2026-04-16 21:21:17 -0700

    mercury

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image";
-import {supabase,supa} from "./supa";
+import {supabase} from "./supa";
 
 export default function Home() {
 async function handleClick() {
