fd2e993233d721c4958820976e5a6422aaa44634
yan73-dev <y1763335@gmail.com>
2026-04-16 21:14:36 -0700

    mercury

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
 "use client"
 import Image from "next/image";
-import supabase from "./supa";
+import {supabase,supa} from "./supa";
 
 export default function Home() {
 async function handleClick() {
-  await supabase.from("yan").insert([{name:"name"}])}
-  return <button onClick={handleClick} style={{color:"red"}}>Insert</button>
+  await supabase.from("yan").insert([{name:"name"}])
+  await supa.from("yan").insert([{name:"name"}])
+
+}
+  return <button onClick={handleClick} style={{color:"red",background:"yellow"}}>Insert</button>
 
 } 
--- app/supa.jsx
+++ app/supa.jsx
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+//import {createBrowserClient} from "@supabase.ssr"
 const url ="https://hfnzmlicxajbwrimultq.supabase.co";
 const publishableKey = "sb_publishable_YSoOTNaYX-q0MYwNf3uy4A_R6-Qp1sT";
 const supabase = createClient(url, publishableKey);
+//const supa = ()=> {createBrowserClient(url, publishableKey);}
 
-export default supabase;
+export {supabase}
